5e681b6935440dd957c3acedbaf26712770ba7a6
mehemmedmemmedli2005 <mehemmed.memmedli.05@gmail.com>
2024-12-24 17:26:03 +0400

    Update product.py

--- app/models/product.py
+++ app/models/product.py
@@ -1,1 +1,9 @@
+from pydantic import BaseModel
+from typing import Optional
 
+class Product(BaseModel):
+    id: Optional[int]
+    name: str
+    description: Optional[str]
+    price: float
+    category_id: int
