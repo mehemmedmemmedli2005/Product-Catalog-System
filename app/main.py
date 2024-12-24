9fd934bbe451638fdb1d7a0fc06c32976d8e5faf
mehemmedmemmedli2005 <mehemmed.memmedli.05@gmail.com>
2024-12-24 18:49:23 +0400

    Delete app directory

--- app/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from app.controllers import product_controller
-
-app = FastAPI()
-
-# Include routers
-app.include_router(product_controller.router, prefix="/api/v1")
--- app/models/product.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class Product(BaseModel):
-    id: Optional[int]
-    name: str
-    description: Optional[str]
-    price: float
-    category_id: int
